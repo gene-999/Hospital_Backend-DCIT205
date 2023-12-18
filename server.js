60fa7bc0ab7c2a6fd92871964488ed3f51e6cd8f
gene-999 <eugenebaidoo5@gmail.com>
2023-12-18 09:27:29 +0000

    created api to find specific patient

--- server.js
+++ server.js
@@ -15,27 +15,34 @@ app.use(express.urlencoded({extended:false}))
 mongoose.connect('mongodb://localhost:27017')
 .then(()=>{
     app.listen(3000)
-})
+});
 
 
 app.post('/add_patients', async (req, res) =>{
     let patient = new Patients(res.body);
     patient = await patient.save();
     res.status(200).send(patient.json());
-})
+});
 
 app.post('/vitals_patients', async (req, res) =>{
     let patient = new Vitals(res.body);
     res.status(200).send(patient.json());
-})
+});
 
 app.post('/visitation_patients', async (req, res) =>{
     let patient = new Vistation(res.body);
     patient = await patient.save();
     res.status(200).send(patient.json());
-})
+});
 
 app.get('/patients',async (req, res) =>{
-    const patient = await Patient.find();
+    const patient = await Patients.find();
     res.status(200).send(patient);
-})
+});
+
+
+app.get('/patients/:id',async (req, res) =>{
+    const {id} = req.params
+    const patient = await Patients.find({id});
+    res.status(200).send(patient.json());
+});
